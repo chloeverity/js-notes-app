5c761d3c6a595b5cbcb241ecff07c58f5184dc7a
chloeverity <chloeverity@hotmail.co.uk>
2018-11-12 17:34:00 +0000

    adding note-controller

--- app/note-controller.js
+++ app/note-controller.js
@@ -1,1 +1,21 @@
-document.getElementById('app').innerHTML = 'howdy'
+(function(exports) {
+  function NoteController(notelist) {
+    this.notelist = notelist
+    this.noteListView = 'hello'
+  }
+
+  NoteController.prototype.addNote = function(text) {
+    this.notelist.addNote(text)
+  }
+
+  NoteController.prototype.createView = function() {
+    this.noteListView = new NoteListView(this.notelist)
+  }
+
+  NoteController.prototype.insertHTML = function() {
+    html = this.noteListView.listNotes()
+    document.getElementById('app').innerHTML = html
+  }
+
+  exports.NoteController = NoteController
+})(this)
